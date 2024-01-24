49b382c0d79ea06ea8bf2d64ef5c58b7a8394822
alexeyrand <alexey.rand@yadnex.ru>
2024-01-24 10:44:04 +0300

    final commit

--- src/main/java/com/alexeyrand/justsymbols/api/SymbolController.java
+++ src/main/java/com/alexeyrand/justsymbols/api/SymbolController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class SymbolController {
 
     private final SymbolsService symbolsService;
